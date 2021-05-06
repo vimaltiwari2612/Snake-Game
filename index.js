ccbe3655d6549f8036484b3e5242e2609b8569cd
Vimal Tiwari <vimaltiwari2612@gmail.com>
2021-05-06 22:49:31 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -130,3 +130,57 @@ window.addEventListener('keydown', e =>{
     }
 
 });
+
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* left swipe */ 
+            currentDirection.x = 0;
+            currentDirection.y = -1;
+        } else {
+            /* right swipe */
+            currentDirection.x = 0;
+            currentDirection.y = 1;
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            /* up swipe */ 
+            currentDirection.x = -1;
+            currentDirection.y = 0;
+        } else { 
+            /* down swipe */
+            currentDirection.x = 1;
+            currentDirection.y = 0;
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
